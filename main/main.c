e0898ae950e8c5c66d4c8d99ea81f646215f5e68
cc32d9 <cc32d9@gmail.com>
2022-12-04 22:30:53 +0100

    work in progress: RPC methods

--- main/main.c
+++ main/main.c
@@ -23,12 +23,18 @@
 #include "esp_log.h"
 #include "esp_system.h"
 #include <sys/time.h>
+#include <esp_task_wdt.h>
 
 #include "pbtx_client.h"
 
+#include "mbedtls/sha256.h"
+#include "pbtx-rpc.pb.h"
+#include <pb_encode.h>
+#include <pb_decode.h>
+
 static const char* TAG = "main";
 
-#define BUFLEN 1024
+#define BUFLEN 4096
 
 static unsigned char buf[BUFLEN];
 
@@ -44,39 +50,66 @@ static void dump_buf( unsigned char *buf, size_t len )
 }
 
 
-void app_main() {
-    pbtx_client_init();
-    size_t olen;
-    int err = pbtx_client_get_public_key(buf, BUFLEN, &olen);
-    if( err != 0 ) {
-        ESP_LOGE(TAG, "pbtx_client_get_public_key returned %d", err);
-    }
-    ESP_LOGI(TAG, "key len: %d\nkey:", olen);
-    dump_buf(buf, olen);
 
-    char* msg = "message message message message message";
-    ESP_LOGI(TAG, "Message:");
-    dump_buf((unsigned char*)msg, strlen(msg));
 
-    struct timeval tv_now;
-    gettimeofday(&tv_now, NULL);
-    int64_t time_start_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
-    
-    err = pbtx_client_sign_data((unsigned char*) msg, strlen(msg), buf, BUFLEN,  &olen);
-    
-    gettimeofday(&tv_now, NULL);
-    int64_t time_stop_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
-
-    if( err != 0 ) {
-        ESP_LOGE(TAG, "pbtx_sigp_sign returned %x", -err);
-    }    
-    else {
-        ESP_LOGI(TAG, "sig len: %d\nsignature:", olen);
-        dump_buf(buf, olen);
+void vTaskCode( void * pvParameters )
+{
+    size_t msgsize;
+    if( pbtx_client_rpc_register_account(buf, BUFLEN, &msgsize) != 0 ) {
+        esp_system_abort("pbtx_client_rpc_register_account error");
     }
 
-    ESP_LOGI(TAG, "Elapsed %f ms", (float)(time_stop_us - time_start_us)/1e3);
+    pbtxrpc_RegisterAccountResponse msg;
+
+    mbedtls_sha256_context md_ctx;
+    mbedtls_sha256_init( &md_ctx );
+    mbedtls_sha256_starts( &md_ctx, 0 );
+    mbedtls_sha256_update( &md_ctx, buf, msgsize );
+    mbedtls_sha256_finish( &md_ctx, msg.request_hash.bytes );
+    mbedtls_sha256_free( &md_ctx );
+    msg.request_hash.size = 32;
+
+    msg.status = 0;
+    msg.network_id = 9223372036854775808ULL;
+    msg.last_seqnum = 4294900000;
+    msg.prev_hash = 9223372036854775808ULL;
+
+    pb_ostream_t respstream = pb_ostream_from_buffer(buf, BUFLEN);
+    if( !pb_encode(&respstream, pbtxrpc_RegisterAccountResponse_fields, &msg) ) {
+        ESP_LOGE(TAG, "RegisterAccountResponse pb_encode error: %s", PB_GET_ERROR(&respstream));
+        esp_system_abort("");
+    }
+
+    if( pbtx_client_rpc_register_account_response(buf, respstream.bytes_written) != 0 ) {
+        esp_system_abort("pbtx_client_rpc_register_account_response error");
+    }
+
+    for( ;; )
+    {
+        struct timeval tv_now;
+        gettimeofday(&tv_now, NULL);
+        int64_t time_start_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+
+        size_t olen;
+        if( pbtx_client_rpc_transaction(4294900000, buf, 2048, buf, BUFLEN, &olen) != 0 ) {
+            esp_system_abort("pbtx_client_rpc_transaction error");
+        }
+
+        ESP_LOGI(TAG, "message length: %d", olen);
+                
+        gettimeofday(&tv_now, NULL);
+        int64_t time_stop_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+                
+        ESP_LOGI(TAG, "Elapsed %f ms", (float)(time_stop_us - time_start_us)/1e3);
+        vTaskDelay(50);
+    }
 }
 
 
+void app_main() {
+    pbtx_client_init();
 
+    TaskHandle_t xHandle = NULL;
+    xTaskCreate( vTaskCode, "NAME", 10000, NULL, 1, &xHandle );
+    configASSERT( xHandle );
+}
