d22fd67821a59537b9a495f2291a07e311f26d49
cc32d9 <cc32d9@gmail.com>
2023-01-15 02:15:34 +0100

    work in progress

--- main/main.c
+++ main/main.c
@@ -19,26 +19,36 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+
 #include <sys/time.h>
 #include <esp_task_wdt.h>
 
 #include "pbtx_client.h"
 
-#include "mbedtls/sha256.h"
-#include "pbtx-rpc.pb.h"
-#include <pb_encode.h>
-#include <pb_decode.h>
 
 static const char* TAG = "main";
+static int s_retry_num = 0;
+
 
 #define BUFLEN 4096
 
 static unsigned char buf[BUFLEN];
 
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
 
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
 
 static void dump_buf( unsigned char *buf, size_t len )
 {
@@ -50,6 +60,108 @@ static void dump_buf( unsigned char *buf, size_t len )
 }
 
 
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < CONFIG_PBTXCLIENT_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_PBTXCLIENT_WIFI_SSID,
+            .password = CONFIG_PBTXCLIENT_WIFI_PASSWORD,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s", CONFIG_PBTXCLIENT_WIFI_SSID);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 CONFIG_PBTXCLIENT_WIFI_SSID, CONFIG_PBTXCLIENT_WIFI_PASSWORD);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+}
+
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+
+    case HTTP_EVENT_ON_CONNECTED:
+        output_len = 0;
+        break;
+        
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+        output_len += evt->data_len;
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
 
 
 void vTaskCode( void * pvParameters )
@@ -59,31 +171,41 @@ void vTaskCode( void * pvParameters )
         esp_system_abort("pbtx_client_rpc_register_account error");
     }
 
-    pbtxrpc_RegisterAccountResponse msg;
+    size_t content_length;
+    
+    char url[128];
+    strcpy(url, CONFIG_PBTXCLIENT_RPC_URL);
+    strcat(url, "/register_account");
 
-    mbedtls_sha256_context md_ctx;
-    mbedtls_sha256_init( &md_ctx );
-    mbedtls_sha256_starts( &md_ctx, 0 );
-    mbedtls_sha256_update( &md_ctx, buf, msgsize );
-    mbedtls_sha256_finish( &md_ctx, msg.request_hash.bytes );
-    mbedtls_sha256_free( &md_ctx );
-    msg.request_hash.size = 32;
+    esp_http_client_config_t config = {
+        .url = url,
+        .user_data = buf,
+        .event_handler = _http_event_handler,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
 
-    msg.status = 0;
-    msg.network_id = 9223372036854775808ULL;
-    msg.last_seqnum = 4294900000;
-    msg.prev_hash = 9223372036854775808ULL;
-
-    pb_ostream_t respstream = pb_ostream_from_buffer(buf, BUFLEN);
-    if( !pb_encode(&respstream, pbtxrpc_RegisterAccountResponse_fields, &msg) ) {
-        ESP_LOGE(TAG, "RegisterAccountResponse pb_encode error: %s", PB_GET_ERROR(&respstream));
-        esp_system_abort("");
+    esp_http_client_set_method(client, HTTP_METHOD_POST);
+    esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
+    esp_http_client_set_post_field(client, (char*)buf, msgsize);
+    int err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        content_length = esp_http_client_get_content_length(client);
+        ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %d",
+                 esp_http_client_get_status_code(client), content_length);
+    } else {
+        ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+        abort();
     }
 
-    if( pbtx_client_rpc_register_account_response(buf, respstream.bytes_written) != 0 ) {
+    dump_buf(buf, content_length);
+    
+    assert(content_length < BUFLEN );        
+    if( pbtx_client_rpc_register_account_response(buf, content_length) != 0 ) {
         esp_system_abort("pbtx_client_rpc_register_account_response error");
     }
 
+
+    /*
     for( ;; )
     {
         struct timeval tv_now;
@@ -103,13 +225,17 @@ void vTaskCode( void * pvParameters )
         ESP_LOGI(TAG, "Elapsed %f ms", (float)(time_stop_us - time_start_us)/1e3);
         vTaskDelay(50);
     }
+*/
 }
 
 
 void app_main() {
-    pbtx_client_init();
+    int err = pbtx_client_init();
+    ESP_ERROR_CHECK(err);
+
+    wifi_init_sta();
 
     TaskHandle_t xHandle = NULL;
-    xTaskCreate( vTaskCode, "NAME", 10000, NULL, 1, &xHandle );
+    xTaskCreate( vTaskCode, "PBTXCLIENT", 10000, NULL, 1, &xHandle );
     configASSERT( xHandle );
 }
